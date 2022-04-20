65db898b2a2ab60401b7f5aa301fe6968a166be4
samet <samet.mollaoglu@hotmail.com>
2022-04-20 16:32:09 +0300

    helloWorld updated

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -2,7 +2,8 @@ import { storage, Context } from "near-sdk-as"
 
 // return the string 'hello world'
 export function helloWorld(): string {
-  return 'hello world'
+  const predecessor = Context.predecessor
+  return 'hello ' + predecessor
 }
 
 // read the given key from account (contract) storage
